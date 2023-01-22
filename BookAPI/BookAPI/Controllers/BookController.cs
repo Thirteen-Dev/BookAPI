237421649c2903a870734ccd40fb47b6f409fcb5
Adriano Moreira <85416850+Thirteen-Dev@users.noreply.github.com>
2023-01-22 20:15:51 -0300

    Add files via upload

---
+++ BookAPI/BookAPI/Controllers/BookController.cs
@@ -0,0 +1,65 @@
+﻿using BookAPI.Model;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetBooks()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id) 
+        
+        {
+            return await _bookRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetBook), new { id = newBook.id }, newBook);
+        }
+        [HttpDelete]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var bookToDelete = await _bookRepository.Get(id);
+            if (bookToDelete == null)
+            return NotFound();
+
+            await _bookRepository.Delete(bookToDelete.id);
+            return NoContent();
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id == book.id)
+                return BadRequest();
+
+                await _bookRepository.Update(book);
+
+
+            return NoContent();
+        } 
+
+
+
+    } 
+}
---
+++ BookAPI/BookAPI/Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookAPI.Model
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string Author { get; set;}
+        public string Description { get; set;}
+        public int Title { get; set;} 
+    }
+}
